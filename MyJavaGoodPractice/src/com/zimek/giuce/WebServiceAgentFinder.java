c297c2accfa73bb98406de8b63ed06d208944907
mzimecki <marcin.zimecki@gmail.com>
2014-01-14 15:26:42 +0100

    Code formatting added

--- MyJavaGoodPractice/src/com/zimek/giuce/WebServiceAgentFinder.java
+++ MyJavaGoodPractice/src/com/zimek/giuce/WebServiceAgentFinder.java
@@ -5,17 +5,16 @@ import java.util.List;
 
 public class WebServiceAgentFinder implements AgentFinder {
 
-	  /**
-	   * This method returns an empty list of agents for compilation sake
-	   * 
-	   * @return An empty list of Agents
-	   */
-	  @Override
-	  public List<Agent> findAllAgents() {
-	    List<Agent> agents = new ArrayList<>();
-	    // Lots of RESTFul based implementation would go here
-	    return agents;
-	  }
-
+	/**
+	 * This method returns an empty list of agents for compilation sake
+	 * 
+	 * @return An empty list of Agents
+	 */
+	@Override
+	public List<Agent> findAllAgents() {
+		List<Agent> agents = new ArrayList<>();
+		// Lots of RESTFul based implementation would go here
+		return agents;
 	}
 
+}
